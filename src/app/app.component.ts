2b4e2a66284ed13df879e197fd2b3648dcd04066
rmolina29 <rmolina@ises.com.co>
2023-12-22 10:46:30 -0500

    21-12-23
    final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,13 +11,30 @@ export class AppComponent implements OnInit {
 
     ngOnInit() {
         this.primengConfig.ripple = true;
-            // // Verificar el contador de ventanas al cargar el componente
-            // const windowCount = this.authService.getWindowCount();
-            // if (windowCount > 3) {
-            // // Puedes cerrar la ventana actual o realizar alguna acción
-            // // Aquí simplemente redirigimos al usuario a una página de error
-            // window.location.href = '/error';
-            // }
+
+        this.primengConfig.setTranslation({
+
+                startsWith: 'Empieza con',
+                contains: 'Contiene',
+                notContains: 'No contiene',
+                endsWith: 'Acaba en',
+                equals: 'Igual',
+                notEquals: 'No igual',
+                noFilter: 'Sin filtro',
+                lt: 'Menor que',
+                matchAll: 'Coincidir con todos',
+                matchAny: 'coincide con cualquiera',
+                apply: 'Aplicar',
+                cancel: 'Cancelar',
+                accept: 'Aceptar',
+                addRule: 'Añadir Regla',
+                removeRule: 'Remover Regla',
+                after: 'Después',
+                before: 'Antes',
+                choose: 'Elegir',
+                clear: 'Limpiar'
+        });
+
     }
 
 
